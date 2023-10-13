758edaf1544a925be0967256a841147c9d3ce22f
TORRY-SKY <ezevictor559@gmail.com>
2023-10-13 21:53:01 +0100

    learning react

--- src/component/RandomComp.js
+++ src/component/RandomComp.js
@@ -3,7 +3,7 @@ function ListITems(){
    const customers = ['Tony', 'Ema', "Jenifer", 'Victory'];
   const  CustomerListItems = customers.map((customer, index)=>{
         console.log(customer);
-        return <li key={index}>{customer }</li>
+        return <li key={index}>{customer}</li>
         
     })
     return <>
