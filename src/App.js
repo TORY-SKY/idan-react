9fdaa229b0303d519f6e0149d7a75ee518a79932
TORRY-SKY <ezevictor559@gmail.com>
2023-10-14 22:05:41 +0100

    context Api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
 import ListITems from './component/RandomComp'
+import React, {useState} from 'react'
+import{LoginContext} from './Context/LoginContext'
 
 function App() {
+   const [showProfile, setShowProfile] = useState(false);
+   const [userName, setUserName] = useState('');
    return <>
-   <ListITems />
-   <ListITems />
+      
+   <LoginContext.Provider >
+      <ListITems />
+      
+   </LoginContext.Provider>
+   
    
    </>
    
