adf3f02680c98efbb9a2a7d1bfb01b21fb96f2e5
TORRY-SKY <ezevictor559@gmail.com>
2023-10-14 04:03:33 +0100

    learning_about_ContextAPI_in_React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
 import './App.css';
 import ListITems from './component/RandomComp'
+import React, {createContext, useState, useContext} from 'react'
+const ContAPI = createContext({
+   user: null
+})
 
 function App() {
    return <>
    <ListITems />
-   <h2>Where are the customers</h2>
-   <p>Everything is under control</p>
+   
    </>
    
 }
 
-
+export const  ContAPI = ContAPI;
 export default App;
 
