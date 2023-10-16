ec23a00d842cdb5a07b6ba4ec1f5fdc8d7871cbc
TORRY-SKY <ezevictor559@gmail.com>
2023-10-16 17:20:28 +0100

    another error aaaaaerrrrrrrrr

--- src/App.js
+++ src/App.js
@@ -3,29 +3,15 @@ import './App.css';
 //import ListITems from './component/RandomComp'
 //import React, {useState} from 'react'
 //import{LoginContext} from './Context/LoginContext'
-import Counter from './component/useState.js'
+//import Counter from './component/useState.js'
 import UseTate from './Context/LoginContext.js'
 
-   function DontPlay(){
-      
-    let DDate = new Date();
-    const ToDay = DDate.getDay();
-    switch(ToDay){
-      case 1: 
-        return 'Its Monday bro';
-        default:
-         return 'Its not Sunday';
-    }
-   }
    
 function App() {
    return <>
    <h1>HELLO WORLD FU**K</h1>
       
-   <Counter />
-   <DontPlay />
-   
-   <UseTate name="Victor" />
+   <UseTate />
    </>
    
 }
--- src/Context/LoginContext.js
+++ src/Context/LoginContext.js
@@ -1,13 +1,13 @@
-import {createContext, useContext, useReduce, useState} from 'react'
+import React from 'react';
+import {useState} from 'react'
 
-function UseTate(props){
-    const [countThis setCountThis] = useState(0);
+function UseTate(){
+    const [count setCount] = useState(0);
     return(
         <>
-        <h1>Care to increase {countThis}</h1>
-        <p>{props.name} clicked the button</p>
-        <button onClick={()=> setCountThis(countThis + 1)}>Increase</button>
-        <button onClick={()=>setCountThis(countThis - 1)}>Decrease</button>
+        <h1>Care to increase {count}</h1>
+        <button onClick={()=>setCount(count + 1)}>Increase</button>
+        <button onClick={()=>setCount(count - 1)}>Decrease</button>
         </>
     )
 }
